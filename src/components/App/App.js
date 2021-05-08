cde3aac93a33d56056c2b7006f8a353f5034b0ef
Denis Pankov <idenispankov@gmail.com>
2021-05-08 16:50:16 -0700

    About Component, fully responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../Header/Header.js';
+import About from '../About/About.js';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <About />
     </div>
   );
 }
