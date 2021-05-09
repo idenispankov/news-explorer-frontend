fc19892ef2dcb19fa5aa39814d6d46af729d383e
Denis Pankov <idenispankov@gmail.com>
2021-05-08 18:56:17 -0700

    NotFound Component, fully reponsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from '../Header/Header.js';
 import About from '../About/About.js';
 import Footer from '../Footer/Footer.js';
+import NotFound from '../NotFound/NotFound.js';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <NotFound />
       <About />
       <Footer />
     </div>
