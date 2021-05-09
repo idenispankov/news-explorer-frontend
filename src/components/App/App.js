71261927d5066c19feb8fdf02fe5b64fdb56372b
Denis Pankov <idenispankov@gmail.com>
2021-05-08 19:14:35 -0700

    SavedNewsHeader Component added, fully responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,13 @@ import About from '../About/About.js';
 import Footer from '../Footer/Footer.js';
 import NotFound from '../NotFound/NotFound.js';
 import Preloader from '../Preloader/Preloader.js';
+import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader.js';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <SavedNewsHeader />
       <NotFound />
       <About />
       <Preloader />
---
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -0,0 +1,15 @@
+import './SavedNewsHeader.css';
+
+const SavedNewsHeader = () => {
+  return (
+    <section className='saved-news'>
+      <p className='saved-news__text'>Saved articles</p>
+      <h2 className='saved-news__heading'>Elise, you have 5 saved articles</h2>
+      <p className='saved-news__footer'>
+        By keywords: Nature, Yellowstone, and 2 other
+      </p>
+    </section>
+  );
+};
+
+export default SavedNewsHeader;
