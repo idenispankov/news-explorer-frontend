6f1cc3dc0032d6940dc30af22b2ccfb794e65531
Denis Pankov <idenispankov@gmail.com>
2021-05-12 20:07:23 -0700

    created input component

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,31 +1,42 @@
 import './PopupWithForm.css';
 import { NavLink } from 'react-router-dom';
 
-const PopupWithForm = ({ isPopupOpen, setIsPopupOpen, setLoggedin }) => {
+const PopupWithForm = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
-    setLoggedin(true);
-    setIsPopupOpen(false);
+    props.setLoggedin(true);
+    props.setIsPopupOpen(false);
   };
 
   const onClose = () => {
-    setIsPopupOpen(false);
+    props.setIsPopupOpen(false);
   };
   return (
-    <div className={`modal ${isPopupOpen && 'modal-open'}`}>
+    <div className={`modal ${props.isPopupOpen && 'modal-open'}`}>
       <form className='form'>
         {/* For Heading props.text later */}
         <h2 className='form__heading'>Sign in</h2>
+        {props.children}
         {/* Input separate component with placeholder, type */}
-        <label className='form__label' htmlFor='email'>
+        {/* <label className='form__label' htmlFor='email'>
           Email
         </label>
-        <input className='form__input' type='text' placeholder='Email' />
+        <input
+          className='form__input'
+          type='text'
+          placeholder='Email'
+          name='email`'
+        /> */}
         {/* <p className='form__input-error'>Invalid email address</p> */}
-        <label className='form__label' htmlFor='password'>
+        {/* <label className='form__label' htmlFor='password'>
           Password
         </label>
-        <input className='form__input' type='text' placeholder='Password' />
+        <input
+          className='form__input'
+          type='text'
+          placeholder='Password'
+          name='password'
+        /> */}
         {/* <p className='form__text-error'>This email is not available</p> */}
         {/* For Button props.buttonText */}
         <button
@@ -38,7 +49,7 @@ const PopupWithForm = ({ isPopupOpen, setIsPopupOpen, setLoggedin }) => {
         {/* props.linkText */}
         <p className='form__text'>
           or{' '}
-          <NavLink to='/signup' className='form__link'>
+          <NavLink to='signup' className='form__link'>
             Sign up
           </NavLink>
         </p>
