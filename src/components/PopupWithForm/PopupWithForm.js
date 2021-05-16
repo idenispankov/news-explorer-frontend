2b93528945235361dbbcb4358fb70f11b168e770
Denis Pankov <idenispankov@gmail.com>
2021-05-16 13:06:03 -0700

    changed css selector for popup from modal-open to boolean modifire modal_is-open

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -2,7 +2,7 @@ import './PopupWithForm.css';
 
 const PopupWithForm = (props) => {
   return (
-    <div className={`modal ${props.isPopupOpen && 'modal-open'}`}>
+    <div className={`modal ${props.isPopupOpen && 'modal_is-open'}`}>
       <form className='form' onSubmit={props.onSubmit} noValidate>
         <h2 className='form__heading'>{props.formHeadingText}</h2>
         {props.children}
