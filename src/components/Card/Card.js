f9895eefb69e69b66bcced29aa4593de592d6d6f
Denis Pankov <idenispankov@gmail.com>
2021-06-03 17:37:13 -0700

    made articles links to external page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -20,28 +20,37 @@ const Card = ({
   };
 
   return (
-    <div className='card'>
-      <img className='card__image' src={article.urlToImage} alt='dog' />
-      <div className='card__description'>
-        <p className='card__date'>{formatDate(article.publishedAt)}</p>
-        <p className='card__title'>{article.title}</p>
-        <p className='card__text'>{article.description}</p>
-        <p className='card__footer'>{article.source.name}</p>
-        <button
-          className={`card__save-icon ${isCardSaved && 'card__saved-icon'}`}
-          onClick={onCardToggle}
-        ></button>
-        {!loggedin && (
-          <>
-            <button
-              className='card__save-icon'
-              onClick={handleSigninClick}
-            ></button>
-            <p className='card__tooltip'>Sign in to save articles</p>
-          </>
-        )}
+    <>
+      <div className='card'>
+        <img className='card__image' src={article.urlToImage} alt='dog' />
+        <div className='card__description'>
+          <a
+            className='card_redirect'
+            href={article.url}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p className='card__date'>{formatDate(article.publishedAt)}</p>
+            <p className='card__title'>{article.title}</p>
+            <p className='card__text'>{article.description}</p>
+            <p className='card__footer'>{article.source.name}</p>
+          </a>
+          <button
+            className={`card__save-icon ${isCardSaved && 'card__saved-icon'}`}
+            onClick={onCardToggle}
+          ></button>
+          {!loggedin && (
+            <>
+              <button
+                className='card__save-icon'
+                onClick={handleSigninClick}
+              ></button>
+              <p className='card__tooltip'>Sign in to save articles</p>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/SavedCard/SavedCard.js
+++ src/components/SavedCard/SavedCard.js
@@ -2,19 +2,36 @@ const SavedCard = ({ item, deleteArticleFromSavedNews }) => {
   const onDelete = () => {
     deleteArticleFromSavedNews(item._id);
   };
+
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString('en', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  };
   return (
-    <div className='card'>
-      <img className='card__image' src={item.image} alt='dog' />
-      <div className='card__description'>
-        <p className='card__date'>{item.date}</p>
-        <p className='card__title'>{item.title}</p>
-        <p className='card__text'>{item.text}</p>
-        <p className='card__footer'>{item.source}</p>
-        <button className={`card__delete-icon`} onClick={onDelete}></button>
-        <p className='card__tooltip'>Remove from saved</p>
-        <p className='card__keyword'>{item.keyword}</p>
+    <>
+      <div className='card'>
+        <img className='card__image' src={item.image} alt='dog' />
+        <div className='card__description'>
+          <a
+            className='card_redirect'
+            href={item.link}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p className='card__date'>{formatDate(item.date)}</p>
+            <p className='card__title'>{item.title}</p>
+            <p className='card__text'>{item.text}</p>
+            <p className='card__footer'>{item.source}</p>
+          </a>
+          <button className={`card__delete-icon`} onClick={onDelete}></button>
+          <p className='card__tooltip'>Remove from saved</p>
+          <p className='card__keyword'>{item.keyword}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
